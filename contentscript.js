ce9458450aea9995447f4f8fa32189227ec6b534
Efe Guler <112385528+anisoft89@users.noreply.github.com>
2023-03-01 13:14:35 +0300

    Update contentscript.js
    Büyük fotoğrafları indirmek için düzenleme.

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,11 @@
  
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    var imagesDiv = document.getElementsByClassName("classifiedDetailThumbList ")[0];
+    var imagesDiv = document.getElementsByClassName("megaPhotoThmbItems")[0];
 		var imageList = imagesDiv.getElementsByTagName("IMG");
 		var links =new Array();
 		for(var i=0;i<imageList.length;i++){
-			links.push(imageList[i].getAttribute("src").replace("thmb_",""));
+			links.push(imageList[i].getAttribute("data-source"));
 		}
     if (request.cmd == "download")
       sendResponse({ilanlar: links});
